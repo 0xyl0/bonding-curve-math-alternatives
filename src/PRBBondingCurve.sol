99deb5444f4bf8d33e5515750240fbfffeb9f47d
0xyl <0xyl@tuta.io>
2025-10-22 10:45:21 +0100

    fix: Use native pow function for PRB math

--- src/PRBBondingCurve.sol
+++ src/PRBBondingCurve.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.24;
 
-import { SD59x18, wrap, unwrap } from "prb-math/SD59x18.sol";
+import { wrap, unwrap, pow as SD59x18_pow } from "prb-math/SD59x18.sol";
 import "./BaseBondingCurve.sol";
 
 import "forge-std/console2.sol";
@@ -9,15 +9,7 @@ contract PRBBondingCurve is BaseBondingCurve {
     constructor(uint256 _alpha, uint256 _beta, uint256 _supply) BaseBondingCurve(_alpha, _beta, _supply) {}
 
     function pow(uint256 _base, uint256 _exponent) public pure override returns (uint256) {
-        // _base ^ _exponent = 2 ^ (_exponent * log_2(_base))
-        /*
-        console2.log("-- pow --");
-        console2.log(toUint(fromUint(_base)), "from -> to base");
-        console2.log(toUint(fromUint(_exponent)), "from -> to exp");
-        console2.log(toUint(ABDKMath64x64.log_2(fromUint(_base))), "log");
-        console2.log(toUint(fromUint(_exponent).mul(ABDKMath64x64.log_2(fromUint(_base)))), "exp");
-        console2.log(toUint(ABDKMath64x64.exp_2(fromUint(_exponent).mul(ABDKMath64x64.log_2(fromUint(_base))))), "return");
-        */
-        return uint256(wrap(int256(_exponent)).mul(wrap(int256(_base)).log2()).exp2().unwrap());
+        // Internally uses 2 ^ (_exponent * log_2(_base))
+        return uint256(SD59x18_pow(wrap(int256(_base)), wrap(int256(_exponent))).unwrap());
     }
 }
